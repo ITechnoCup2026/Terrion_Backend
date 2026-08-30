10579dff7717674eba0832b4fab5214a572c1b4c
BRYAN1309 <bryanthanaya1@gmail.com>
2026-08-30 11:00:17 +0700

    feat: add Supabase Postgres GORM connection config

---
+++ internal/config/gorm.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func buildDSN(cfg *Config) string {
+	return fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=%s",
+		cfg.Database.Host,
+		cfg.Database.User,
+		cfg.Database.Password,
+		cfg.Database.Name,
+		cfg.Database.Port,
+		cfg.Database.SSLMode,
+	)
+}
+
+// NewDatabase opens a connection to Supabase Postgres (session pooler /
+// direct connection) and configures the connection pool.
+func NewDatabase(cfg *Config, log *logrus.Logger) *gorm.DB {
+	dsn := buildDSN(cfg)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.New(&logrusWriter{Logger: log}, logger.Config{
+			SlowThreshold:             time.Second * 5,
+			Colorful:                  false,
+			IgnoreRecordNotFoundError: true,
+			ParameterizedQueries:      true,
+			LogLevel:                  logger.Info,
+		}),
+	})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+
+	connection, err := db.DB()
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+
+	connection.SetMaxIdleConns(cfg.Database.PoolIdle)
+	connection.SetMaxOpenConns(cfg.Database.PoolMax)
+	connection.SetConnMaxLifetime(time.Second * time.Duration(cfg.Database.PoolLifetime))
+
+	return db
+}
+
+type logrusWriter struct {
+	Logger *logrus.Logger
+}
+
+func (l *logrusWriter) Printf(message string, args ...interface{}) {
+	l.Logger.Tracef(message, args...)
+}
